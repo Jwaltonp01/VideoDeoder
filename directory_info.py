09d29829b1419b4b1809ceb2787c3e9f06a3e1cc
JerryWP <22497456+Jwaltonp01@users.noreply.github.com>
2019-04-09 15:40:31 +0900

    Update directory_info.py
    Minor logic revision

--- directory_info.py
+++ directory_info.py
@@ -1,5 +1,27 @@
+# coding=utf-8
 import os
+
+# Project Root Directory
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Project Name
+PROJECT_DIR_NAME = "VideoDecoder"
 
 
 def get_project_dir():
-    return os.path.dirname(os.getcwd()) + "/VideoDecoder/"
+    """
+    Returns the project root directory
+    :return:
+        Project path
+    """
+    global ROOT_DIR
+
+    paths = ROOT_DIR.split("/")
+    paths = list(filter(lambda a: a != '', paths))
+
+    if PROJECT_DIR_NAME not in paths[-1]:
+        ROOT_DIR = ROOT_DIR + PROJECT_DIR_NAME
+
+    if ROOT_DIR[-1] != "/":
+        ROOT_DIR = ROOT_DIR + "/"
+
+return ROOT_DIR
